21ec8f9ddebbc1671a836f5ab454ba1e13f11124
henninghk <64491564+henninghk@users.noreply.github.com>
2021-04-22 14:24:18 +0200

    Initial commit

---
+++ bank/FootBallTeam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOtask
+{
+    class FootBallTeam
+    {
+        public string _teamName;
+        public string _coach;
+        public int _tablePlace;
+
+        public void Show()
+        {
+            Console.WriteLine($"laget er {_teamName},trener er {_coach}, de er på plass nr{_tablePlace}");
+            
+        }
+
+        public FootBallTeam(string teamName, string coach, int tablePlace)
+        {
+            _teamName = teamName;
+            _coach = coach;
+            _tablePlace = tablePlace;
+
+        }
+    }
+}
---
+++ bank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOtask
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var team = new FootBallTeam("Liverpool", "klopp", 1);
+            var team1 = new FootBallTeam("manchesterU", "solskjær", 20);
+            team.Show();
+            team1.Show();
+            
+
+        }
+    }
+}
